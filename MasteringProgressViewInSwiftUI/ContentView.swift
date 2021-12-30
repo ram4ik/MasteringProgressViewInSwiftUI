85210a899a59c8cec4356bbca1ff3405810cc9b8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-30 20:45:48 +0200

    Initial Commit

---
+++ MasteringProgressViewInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MasteringProgressViewInSwiftUI
+//
+//  Created by ramil on 30.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MasteringProgressViewInSwiftUI/MasteringProgressViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringProgressViewInSwiftUIApp.swift
+//  MasteringProgressViewInSwiftUI
+//
+//  Created by ramil on 30.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct MasteringProgressViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
